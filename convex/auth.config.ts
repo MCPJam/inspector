c1e3d8b4931645d96ca2b0cf1b692f801fd7a6dc
marcelo <marcelojimenezrocabado@gmail.com>
2025-09-04 22:12:09 -0700

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { convexAuth, customJwt } from "convex/server";
 
 // WorkOS AuthKit issues JWTs whose JWK set is available at:
@@ -13,4 +14,29 @@ export default convexAuth({
     }),
   ],
 });
+=======
+const clientId = process.env.WORKOS_CLIENT_ID;
+
+const authConfig = {
+	providers: [
+		{
+			type: 'customJwt',
+			issuer: `https://api.workos.com/`,
+			algorithm: 'RS256',
+			jwks: `https://api.workos.com/sso/jwks/${clientId}`,
+			applicationID: clientId,
+		},
+		{
+			type: 'customJwt',
+			issuer: `https://api.workos.com/user_management/${clientId}`,
+			algorithm: 'RS256',
+			jwks: `https://api.workos.com/sso/jwks/${clientId}`,
+			applicationID: clientId,
+		},
+	],
+};
+
+export default authConfig;
+
+>>>>>>> Stashed changes
 
