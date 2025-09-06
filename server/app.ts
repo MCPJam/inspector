278b695b47eaee7a7ea9e94a37a3cf649d78fa62
marcelo <marcelojimenezrocabado@gmail.com>
2025-09-05 20:02:41 -0700

    fix desktop prod port

--- server/app.ts
+++ server/app.ts
@@ -36,7 +36,7 @@ export function createHonoApp() {
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:5173",
-        "http://localhost:17692",
+        "http://localhost:3000",
       ],
       credentials: true,
     }),
