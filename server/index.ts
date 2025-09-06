14202c8ff83ffe584e39aa376cba266a516e2e0e
marcelo <marcelojimenezrocabado@gmail.com>
2025-09-05 19:22:13 -0700

    fix sign in via npm start

--- server/index.ts
+++ server/index.ts
@@ -196,8 +196,8 @@ if (process.env.NODE_ENV === "production") {
 
 const port = parseInt(process.env.PORT || "3000");
 
-// Display the localhost URL in a box
-logBox(`http://localhost:${port}`, "🚀 Inspector Launched");
+// Display the 127.0.0.1 URL in a box
+logBox(`http://127.0.0.1:${port}`, "🚀 Inspector Launched");
 
 // Graceful shutdown handling
 const server = serve({
