3a75482decece3b70af4414c2a3ce16a8bed295a
Matthew Wang <matt8p@Matthews-MacBook-Pro.local>
2025-10-20 18:31:40 -0700

    server analytics

--- client/src/components/ServersTab.tsx
+++ client/src/components/ServersTab.tsx
@@ -56,6 +56,7 @@ export function ServersTab({
       location: "servers_tab",
       platform: detectPlatform(),
       environment: detectEnvironment(),
+      num_servers: Object.keys(connectedServerConfigs).length,
     });
   }, []);
 
