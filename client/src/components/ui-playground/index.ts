89576524f54801ea1baf158f079122c110cf66fc
marcelo <marcelojimenezrocabado@gmail.com>
2025-12-06 11:15:31 -0800

    rm barrel

--- client/src/components/ui-playground/index.ts
+++
@@ -1,4 +0,0 @@
-export { UIPlaygroundTab } from "./UIPlaygroundTab";
-export { PlaygroundLeft } from "./PlaygroundLeft";
-export { PlaygroundMain } from "./PlaygroundMain";
-export { PlaygroundRight } from "./PlaygroundRight";
