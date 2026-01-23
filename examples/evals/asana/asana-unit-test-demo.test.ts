abe1c34d66f2782334fe2ba87cb76b080396ff1a
Matthew Wang <matt8p@Matthews-MacBook-Pro.local>
2026-01-22 20:35:12 -0800

    asana evals

--- examples/evals/asana/asana-unit-test-demo.test.ts
+++ examples/evals/asana/asana-unit-test-demo.test.ts
@@ -2,18 +2,31 @@ import { MCPClientManager } from "@mcpjam/sdk";
 
 describe("test oauth token handling", () => {
   test("valid oauth token successfully connects to server", async () => {
-    console.log("ASANA_TOKEN", process.env.ASANA_TOKEN);
-    const clientManager = new MCPClientManager({
-      asana: {
-        url: new URL("https://mcp.asana.com/sse"),
-        requestInit: {
-          headers: {
-            Authorization: `Bearer ${process.env.ASANA_TOKEN}`,
-          },
+    const clientManager = new MCPClientManager();
+    await clientManager.connectToServer("asana", {
+      url: new URL("https://mcp.asana.com/sse"),
+      requestInit: {
+        headers: {
+          Authorization: `Bearer ${process.env.ASANA_TOKEN}`,
         },
       },
     });
-    console.log(await clientManager.listTools("asana"));
-    expect(clientManager.getConnectionStatus("asana")).toBe("connected");
+    expect(await clientManager.getConnectionStatus("asana")).toBe("connected");
+    await clientManager.disconnectServer("asana");
+  });
+
+  test("invalid oauth token fails to connect to server", async () => {
+    const config = {
+      url: new URL("https://mcp.asana.com/sse"),
+      requestInit: {
+        headers: {
+          Authorization: `Bearer abcxyz`,
+        },
+      },
+    };
+    const clientManager = new MCPClientManager();
+    await expect(
+      clientManager.connectToServer("asana", config)
+    ).rejects.toThrow();
   });
 });
