ignaciojimenezr/mcpjam

Key servers by UUID

mcpjam-inspector/client/src/components/ChatTabV2.tsx
@@ -33,7 +33,7 @@ import { addTokenToUrl, authFetch } from "@/lib/session-token";
 
 interface ChatTabProps {
   connectedServerConfigs: Record<string, ServerWithName>;
-  selectedServerNames: string[];
+  selectedServerIds: string[];
   onHasMessagesChange?: (hasMessages: boolean) => void;
 }
 
@@ -57,7 +57,7 @@ function ScrollToBottomButton() {
 
 export function ChatTabV2({
   connectedServerConfigs,
-  selectedServerNames,
+  selectedServerIds,
   onHasMessagesChange,
 }: ChatTabProps) {
   const { signUp } = useAuth();
@@ -89,15 +89,15 @@ export function ChatTabV2({
   const [isWidgetFullscreen, setIsWidgetFullscreen] = useState(false);
 
   // Filter to only connected servers
-  const selectedConnectedServerNames = useMemo(
+  const selectedConnectedServerIds = useMemo(
     () =>
-      selectedServerNames.filter(
-        (name) =>
-          connectedServerConfigs[name]?.connectionStatus === "connected",
+      selectedServerIds.filter(
+        (id) =>
+          connectedServerConfigs[id]?.connectionStatus === "connected",
       ),
-    [selectedServerNames, connectedServerConfigs],
+    [selectedServerIds, connectedServerConfigs],
   );
-  const noServersConnected = selectedConnectedServerNames.length === 0;
+  const noServersConnected = selectedConnectedServerIds.length === 0;
 
   // Use shared chat session hook
   const {
@@ -129,7 +129,7 @@ export function ChatTabV2({
     disableForAuthentication,
     submitBlocked: baseSubmitBlocked,
   } = useChatSession({
-    selectedServers: selectedConnectedServerNames,
+    selectedServers: selectedConnectedServerIds,
     onReset: () => {
       setInput("");
       setWidgetStateQueue([]);
@@ -144,15 +144,15 @@ export function ChatTabV2({
   // Server instructions
   const selectedServerInstructions = useMemo(() => {
     const instructions: Record<string, string> = {};
-    for (const serverName of selectedServerNames) {
-      const server = connectedServerConfigs[serverName];
+    for (const serverId of selectedServerIds) {
+      const server = connectedServerConfigs[serverId];
       const instruction = server?.initializationInfo?.instructions;
       if (instruction) {
-        instructions[serverName] = instruction;
+        instructions[serverId] = instruction;
       }
     }
     return instructions;
-  }, [connectedServerConfigs, selectedServerNames]);
+  }, [connectedServerConfigs, selectedServerIds]);
 
   // Keep server instruction system messages in sync with selected servers
   useEffect(() => {
@@ -168,17 +168,21 @@ export function ChatTabV2({
 
       const instructionMessages = Object.entries(selectedServerInstructions)
         .sort(([a], [b]) => a.localeCompare(b))
-        .map(([serverName, instruction]) => ({
-          id: `server-instruction-${serverName}`,
-          role: "system" as const,
-          parts: [
-            {
-              type: "text" as const,
-              text: `Server ${serverName} instructions: ${instruction}`,
-            },
-          ],
-          metadata: { source: "server-instruction", serverName },
-        }));
+        .map(([serverId, instruction]) => {
+          const displayName =
+            connectedServerConfigs[serverId]?.name ?? serverId;
+          return {
+            id: `server-instruction-${serverId}`,
+            role: "system" as const,
+            parts: [
+              {
+                type: "text" as const,
+                text: `Server ${displayName} instructions: ${instruction}`,
+              },
+            ],
+            metadata: { source: "server-instruction", serverName: displayName },
+          };
+        });
 
       return [...instructionMessages, ...filtered];
     });
@@ -466,7 +470,7 @@ export function ChatTabV2({
     onResetChat: baseResetChat,
     submitDisabled: submitBlocked,
     tokenUsage,
-    selectedServers: selectedConnectedServerNames,
+    selectedServers: selectedConnectedServerIds,
     mcpToolsTokenCount,
     mcpToolsTokenCountLoading,
     connectedServerConfigs,

mcpjam-inspector/client/src/components/ui-playground/PlaygroundMain.tsx
@@ -159,7 +159,8 @@ const CSP_MODE_OPTIONS: {
 ];
 
 interface PlaygroundMainProps {
-  serverName: string;
+  serverId: string;
+  serverName?: string;
   onWidgetStateChange?: (toolCallId: string, state: unknown) => void;
   // Execution state for "Invoking" indicator
   isExecuting?: boolean;
@@ -231,6 +232,7 @@ function InvokingIndicator({
 }
 
 export function PlaygroundMain({
+  serverId,
   serverName,
   onWidgetStateChange,
   isExecuting,
@@ -299,12 +301,14 @@ export function PlaygroundMain({
   }, [themeMode, setThemeMode]);
 
   const { servers } = useSharedAppState();
+  const displayServerName =
+    serverName ?? servers[serverId]?.name ?? serverId ?? "";
   const selectedServers = useMemo(
     () =>
-      serverName && servers[serverName]?.connectionStatus === "connected"
-        ? [serverName]
+      serverId && servers[serverId]?.connectionStatus === "connected"
+        ? [serverId]
         : [],
-    [serverName, servers],
+    [serverId, servers],
   );
 
   // Use shared chat session hook
@@ -556,7 +560,9 @@ export function PlaygroundMain({
     selectedServers,
     mcpToolsTokenCount: null,
     mcpToolsTokenCountLoading: false,
-    connectedServerConfigs: { [serverName]: { name: serverName } },
+    connectedServerConfigs: displayServerName
+      ? { [serverId]: { name: displayServerName } }
+      : {},
     systemPromptTokenCount: null,
     systemPromptTokenCountLoading: false,
     mcpPromptResults,

MCPJam/inspector

mcpjam-inspector/client/src/components/ChatTabV2.tsx
@@ -41,7 +41,7 @@ import { XRaySnapshotView } from "@/components/xray/xray-snapshot-view";
 
 interface ChatTabProps {
   connectedOrConnectingServerConfigs: Record<string, ServerWithName>;
-  selectedServerNames: string[];
+  selectedServerIds: string[];
   onHasMessagesChange?: (hasMessages: boolean) => void;
 }
 
@@ -65,7 +65,7 @@ function ScrollToBottomButton() {
 
 export function ChatTabV2({
   connectedOrConnectingServerConfigs,
-  selectedServerNames,
+  selectedServerIds,
   onHasMessagesChange,
 }: ChatTabProps) {
   const { signUp } = useAuth();
@@ -102,15 +102,15 @@ export function ChatTabV2({
   const [xrayMode, setXrayMode] = useState(false);
 
   // Filter to only connected servers
-  const selectedConnectedServerNames = useMemo(
+  const selectedConnectedServerIds = useMemo(
     () =>
-      selectedServerNames.filter(
-        (name) =>
-          connectedOrConnectingServerConfigs[name]?.connectionStatus ===
-          "connected",
+      selectedServerIds.filter(
+        (id) =>
+          connectedOrConnectingServerConfigs[id]?.connectionStatus === "connected",
       ),
-    [selectedServerNames, connectedOrConnectingServerConfigs],
+    [selectedServerIds, connectedOrConnectingServerConfigs],
   );
+  const noServersConnected = selectedConnectedServerIds.length === 0;
 
   // Use shared chat session hook
   const {
@@ -142,7 +142,7 @@ export function ChatTabV2({
     disableForAuthentication,
     submitBlocked: baseSubmitBlocked,
   } = useChatSession({
-    selectedServers: selectedConnectedServerNames,
+    selectedServers: selectedConnectedServerIds,
     onReset: () => {
       setInput("");
       setWidgetStateQueue([]);
@@ -157,15 +157,15 @@ export function ChatTabV2({
   // Server instructions
   const selectedServerInstructions = useMemo(() => {
     const instructions: Record<string, string> = {};
-    for (const serverName of selectedServerNames) {
-      const server = connectedOrConnectingServerConfigs[serverName];
+    for (const serverId of selectedServerIds) {
+      const server = connectedOrConnectingServerConfigs[serverId];
       const instruction = server?.initializationInfo?.instructions;
       if (instruction) {
-        instructions[serverName] = instruction;
+        instructions[serverId] = instruction;
       }
     }
     return instructions;
-  }, [connectedOrConnectingServerConfigs, selectedServerNames]);
+  }, [connectedOrConnectingServerConfigs, selectedServerIds]);
 
   // Keep server instruction system messages in sync with selected servers
   useEffect(() => {
@@ -181,17 +181,21 @@ export function ChatTabV2({
 
       const instructionMessages = Object.entries(selectedServerInstructions)
         .sort(([a], [b]) => a.localeCompare(b))
-        .map(([serverName, instruction]) => ({
-          id: `server-instruction-${serverName}`,
-          role: "system" as const,
-          parts: [
-            {
-              type: "text" as const,
-              text: `Server ${serverName} instructions: ${instruction}`,
-            },
-          ],
-          metadata: { source: "server-instruction", serverName },
-        }));
+        .map(([serverId, instruction]) => {
+          const displayName =
+            connectedServerConfigs[serverId]?.name ?? serverId;
+          return {
+            id: `server-instruction-${serverId}`,
+            role: "system" as const,
+            parts: [
+              {
+                type: "text" as const,
+                text: `Server ${displayName} instructions: ${instruction}`,
+              },
+            ],
+            metadata: { source: "server-instruction", serverName: displayName },
+          };
+        });
 
       return [...instructionMessages, ...filtered];
     });
@@ -497,7 +501,7 @@ export function ChatTabV2({
     onResetChat: baseResetChat,
     submitDisabled: submitBlocked,
     tokenUsage,
-    selectedServers: selectedConnectedServerNames,
+    selectedServers: selectedConnectedServerIds,
     mcpToolsTokenCount,
     mcpToolsTokenCountLoading,
     connectedOrConnectingServerConfigs,

mcpjam-inspector/client/src/components/ui-playground/PlaygroundMain.tsx
@@ -166,7 +166,8 @@ const CSP_MODE_OPTIONS: {
 ];
 
 interface PlaygroundMainProps {
-  serverName: string;
+  serverId: string;
+  serverName?: string;
   onWidgetStateChange?: (toolCallId: string, state: unknown) => void;
   // Execution state for "Invoking" indicator
   isExecuting?: boolean;
@@ -238,6 +239,7 @@ function InvokingIndicator({
 }
 
 export function PlaygroundMain({
+  serverId,
   serverName,
   onWidgetStateChange,
   isExecuting,
@@ -309,12 +311,14 @@ export function PlaygroundMain({
   }, [themeMode, setThemeMode]);
 
   const { servers } = useSharedAppState();
+  const displayServerName =
+    serverName ?? servers[serverId]?.name ?? serverId ?? "";
   const selectedServers = useMemo(
     () =>
-      serverName && servers[serverName]?.connectionStatus === "connected"
-        ? [serverName]
+      serverId && servers[serverId]?.connectionStatus === "connected"
+        ? [serverId]
         : [],
-    [serverName, servers],
+    [serverId, servers],
   );
 
   // Use shared chat session hook
@@ -573,7 +577,9 @@ export function PlaygroundMain({
     selectedServers,
     mcpToolsTokenCount: null,
     mcpToolsTokenCountLoading: false,
-    connectedOrConnectingServerConfigs: { [serverName]: { name: serverName } },
+    connectedOrConnectingServerConfigs: displayServerName
+      ? { [serverId]: { name: displayServerName } }
+      : {},
     systemPromptTokenCount: null,
     systemPromptTokenCountLoading: false,
     mcpPromptResults,